f0e7f84ea60a01a7c47641f7e1db1bcffaab2151
Marco Poletto <marco@marcopoletto.eu>
2019-02-24 17:28:25 +0000

    fixed logo click bug

--- src/components/commons/Header.js
+++ src/components/commons/Header.js
@@ -1,16 +1,19 @@
-import React, { Component } from 'react';
-import MediaQuery from 'react-responsive';
+import React, { Component } from 'react'
+import MediaQuery from 'react-responsive'
+import { Link } from 'gatsby'
 
-import Navigation from './Navitation';
-import MenuItem from './MenuItem';
-import logo from '../../images/logo.svg';
+import Navigation from './Navitation'
+import MenuItem from './MenuItem'
+import logo from '../../images/logo.svg'
 
 class Header extends Component {
     render() {
         return (
             <div className="header__container">
                 <div className="logo">
-                    <a href="/" title="Back to the Homepage"><img src={logo} alt="logo" /></a>
+                    <Link to="/" title="Back to the Homepage">
+                        <img src={logo} alt="logo" />
+                    </Link>
                 </div>
                 <MediaQuery query="(min-width: 1024px)">
                     <Navigation />
@@ -25,4 +28,4 @@ class Header extends Component {
     }
 }
 
-export default Header;
+export default Header
